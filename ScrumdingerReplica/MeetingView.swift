14c34f142a56a4ce3d36c27ff1998dd4beb31fa0
thulanimtetwa <thulanimtetwa@gmail.com>
2025-03-21 05:00:12 +0200

    add meeting view

---
+++ ScrumdingerReplica/MeetingView.swift
@@ -0,0 +1,59 @@
+//
+//  MeetingView.swift
+//  ScrumdingerReplica
+//
+//  Created by Thulani Mtetwa on 2025/03/19.
+//
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            TimeRemainingView()
+            Circle()
+                .strokeBorder(lineWidth: 24)
+            NextSpeakerView()
+        }
+        .padding()
+    }
+}
+
+struct MeetingView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeetingView()
+    }
+}
+
+struct TimeRemainingView: View {
+    var body: some View {
+        HStack {
+            VStack(alignment: .leading) {
+                Text("Seconds Elapsed")
+                    .font(.caption)
+                Label("300", systemImage: "hourglass.tophalf.fill")
+            }
+            Spacer()
+            VStack(alignment: .trailing) {
+                Text("Seconds Remaining")
+                    .font(.caption)
+                Label("600", systemImage: "hourglass.bottomhalf.fill")
+            }
+        }
+        .accessibilityElement(children: .ignore)
+        .accessibilityLabel("Time remaining")
+        .accessibilityValue("10 minutes")
+    }
+}
+
+struct NextSpeakerView: View {
+    var body: some View {
+        HStack {
+            Text("Speaker 1 of 3")
+            Button(action: {}) {
+                Image(systemName: "forward.fill")
+            }
+            .accessibilityLabel("Next speaker")
+        }
+    }
+}
--- ScrumdingerReplica/ScrumdingerReplicaApp.swift
+++ ScrumdingerReplica/ScrumdingerReplicaApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumdingerReplicaApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            // Rendered view
         }
     }
 }
